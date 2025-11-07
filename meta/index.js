6a33bba429063a42c7ee7a5a531511501c2028aa
hasaam303 <Butthasaam05@gmail.com>
2025-11-06 19:54:58 -0800

    Update index.js

--- meta/index.js
+++ meta/index.js
@@ -4,9 +4,5 @@ import "../global.js";
 document.addEventListener("DOMContentLoaded", () => {
   // Create the main content wrapper
   const main = document.createElement("main");
-  main.innerHTML = `
-    <h1>Meta</h1>
-    <p>This page includes stats about the code of this website.</p>
-  `;
   document.body.appendChild(main);
 });
